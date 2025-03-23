10d926c6103bb9345e7e86f0e0bc5a665847ada4
haroldlaw <g00438467@atu.ie>
2025-03-23 13:11:47 +0000

    NavBar Menu

--- frontend/src/components/Common/NavBar.jsx
+++ frontend/src/components/Common/NavBar.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom'
 import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from 'react-icons/hi2'
 import Search from './Search'
 import CartDrawer from '../Layout/CartDrawer'
+import { IoMdClose } from "react-icons/io"
 
 const NavBar = () => {
     const [drawerOpen, setDrawerOpen] = useState(false);
+    const [navDrawerOpen, setNavDrawerOpen] = useState(false);
 
     const toggleCartDrawer = () => {
         setDrawerOpen(!drawerOpen);
+    };
+
+    const toggleNavCartDrawer = () => {
+        setNavDrawerOpen(!navDrawerOpen);
     };
 
     return (
@@ -43,12 +49,34 @@ const NavBar = () => {
                     <div className='overflow-hidden'>
                         <Search />
                     </div>
-                    <button className='md:hidden'>
+                    <button onClick={toggleNavCartDrawer} className='md:hidden'>
                         <HiBars3BottomRight className='h-6 w-6 text-700' />
                     </button>
                 </div>
             </nav>
-            <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+            <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+            {/* Mobile Navigation  */}
+            <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                <div className="flex justify-end p-4">
+                    <button onClick={toggleNavCartDrawer}>
+                        <IoMdClose className="h-6 w-6 text-gray-600" />
+                    </button>
+                </div>
+                <div className='p-4'>
+                    <h2 className="text-xl font-semibold mb-4">Menu</h2>
+                    <nav className='space-y-4'>
+                        <Link to="#" onClick={toggleNavCartDrawer} className="block text-gray-600 hover:text-black">
+                            Digital Cameras
+                        </Link>
+                        <Link to="#" onClick={toggleNavCartDrawer} className="block text-gray-600 hover:text-black">
+                            Film Cameras
+                        </Link>
+                        <Link to="#" onClick={toggleNavCartDrawer} className="block text-gray-600 hover:text-black">
+                            Lenses
+                        </Link>
+                    </nav>
+                </div>
+            </div>
         </div>
     )
 }
