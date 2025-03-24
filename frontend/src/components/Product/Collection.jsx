4e85db2e0c919923f7d4f072078644cc84d0e701
haroldlaw <g00438467@atu.ie>
2025-03-24 14:05:22 +0000

    Collection section

---
+++ frontend/src/components/Product/Collection.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import digitalCollectionImage from "../../assets/digital.jpg";
+import filmCollectionImage from "../../assets/film.jpg";
+import lensesCollectionImage from "../../assets/lenses.jpg";
+import { Link } from 'react-router-dom';
+
+const Collection = () => {
+    return (
+        <section className='py-16 px-4 lg:px-0'>
+            <div className='container mx-auto flex flex-col md:flex-row gap-8'>
+                {/* Digital Camera Collection */}
+                <div className="relative flex-1">
+                    <img
+                        src={digitalCollectionImage}
+                        alt="Digital Camera Collection"
+                        className="w-full h-[700px] object-cover"
+                    />
+                    <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
+                        <h2 className="text-2xl font-bold text-gray-900 mb-3">
+                            Digital Cameras
+                        </h2>
+                        <Link
+                            to="/collections/all?digital"
+                            className="text-gray-900 underline"
+                        >
+                            Shop Now
+                        </Link>
+                    </div>
+                </div>
+                {/* Film Camera Collection */}
+                <div className="relative flex-1">
+                    <img
+                        src={filmCollectionImage}
+                        alt="Film Camera Collection"
+                        className="w-full h-[700px] object-cover"
+                    />
+                    <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
+                        <h2 className="text-2xl font-bold text-gray-900 mb-3">
+                            Film Cameras
+                        </h2>
+                        <Link
+                            to="/collections/all?film"
+                            className="text-gray-900 underline"
+                        >
+                            Shop Now
+                        </Link>
+                    </div>
+                </div>
+                {/* Lenses Collection */}
+                <div className="relative flex-1">
+                    <img
+                        src={lensesCollectionImage}
+                        alt="Lenses Collection"
+                        className="w-full h-[700px] object-cover"
+                    />
+                    <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
+                        <h2 className="text-2xl font-bold text-gray-900 mb-3">
+                            Lenses
+                        </h2>
+                        <Link
+                            to="/collections/all?lenses"
+                            className="text-gray-900 underline"
+                        >
+                            Shop Now
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Collection
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main from '../components/Layout/Main'
+import Collection from '../components/Product/Collection'
 
 const Home = () => {
   return (
     <div>
         <Main />
+        <Collection />
     </div>
   )
 }
