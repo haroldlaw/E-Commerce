9c4d5041396c80dadd8dfdfa57e062286e5e3943
haroldlaw <g00438467@atu.ie>
2025-03-25 14:25:47 +0000

    Order page

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Order = () => {
-  return (
-    <div>Order</div>
-  )
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            const mockOrder = [{
+                id: "001",
+                createdAt: new Date(),
+                shippingAddress: { city: "Galway", country: "Ireland" },
+                items: [{
+                    name: "Lenses"
+                }],
+                price: 100,
+                isPaid: true
+            }]
+            setOrders(mockOrder)
+        })
+    }, 1000)
+
+    return (
+        <div className='max-w-7xl mx-auto p-4 sm:p-6'>
+            <h2 className='text-xl sm:text-2xl font-bold mb-6'>
+                Orders
+            </h2>
+            <div className='relative shadow-md sm:rounded-lg overflow-hidden'>
+                <table className='min-w-full text-left text-gray-500'>
+                    <thead className='bg-gray-100 text-xs uppercase text-gray-700'>
+                        <tr>
+                            <th className="py-2 px-4 sm:py-3">Order ID</th>
+                            <th className="py-2 px-4 sm:py-3">Order Placed</th>
+                            <th className="py-2 px-4 sm:py-3">Shipping Address</th>
+                            <th className="py-2 px-4 sm:py-3">Quantity</th>
+                            <th className="py-2 px-4 sm:py-3">Price</th>
+                            <th className="py-2 px-4 sm:py-3">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.length > 0 ? (
+                            orders.map((order) => (
+                                <tr
+                                    key={order.id}
+                                    className="border-b hover:border-gray-50 cursor-pointer"
+                                >
+                                    <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap">
+                                        {order.id}
+                                    </td>
+                                    <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900">
+                                        {new Date(order.createdAt).toLocaleDateString()}{" "}
+                                        {new Date(order.createdAt).toLocaleTimeString()}
+                                    </td>
+                                    <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900">
+                                        {order.shippingAddress
+                                            ? `${order.shippingAddress.city}, ${order.shippingAddress.country}`
+                                            : "N/A"}
+                                    </td>
+                                    <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap">
+                                        {order.items.length}
+                                    </td>
+                                    <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap">
+                                        ${order.price}
+                                    </td>
+                                    <td className="py-2 px-2 sm:py-4 sm:px-4">
+                                        <span
+                                            className={`${order.isPaid
+                                                    ? "bg-green-100 text-green-700"
+                                                    : "bg-red-100 text-red-700"
+                                                } px-2 py-1 rounded-full text-xs sm:text-sm font-medium `}
+                                        >
+                                            {order.isPaid ? "Paid" : "Pending"}
+                                        </span>
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan={7} className="py-4 px-4 text-center text-gray-500">
+                                    No Orders
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default Order
