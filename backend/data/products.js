09a67d03468b5d168f3af2691bdafc1e36812019
haroldlaw <g00438467@atu.ie>
2025-03-29 15:49:33 +0000

    populate products

---
+++ backend/data/products.js
@@ -0,0 +1,124 @@
+const products = [
+    {
+        name: "Sony a6400",
+        description:
+            "Sony a6400",
+        price: 800,
+        discountPrice: 700,
+        countInStock: 20,
+        sku: "OX-SH-001",
+        category: "Mirrorless camera",
+        brand: "Sony",
+        collection: "Digital camera",
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=39",
+                altText: "a6400",
+            }
+        ],
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        name: "Sony a6700",
+        description:
+            "Sony a6700",
+        price: 900,
+        discountPrice: 800,
+        countInStock: 10,
+        sku: "OX-SH-002",
+        category: "Mirrorless camera",
+        brand: "Sony",
+        collection: "Digital camera",
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=38",
+                altText: "a6700",
+            }
+        ],
+        rating: 4.0,
+        numReviews: 10,
+    },
+    {
+        name: "Sony a6100",
+        description:
+            "Sony a6100",
+        price: 500,
+        discountPrice: 400,
+        countInStock: 10,
+        sku: "OX-SH-003",
+        category: "Mirrorless camera",
+        brand: "Sony",
+        collection: "Digital camera",
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=36",
+                altText: "a6100",
+            }
+        ],
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        name: "Sony a6000",
+        description:
+            "Sony a6000",
+        price: 600,
+        discountPrice: 500,
+        countInStock: 25,
+        sku: "OX-SH-004",
+        category: "Mirrorless camera",
+        brand: "Sony",
+        collection: "Digital camera",
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=37",
+                altText: "a6000",
+            }
+        ],
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        name: "Sony a7c",
+        description:
+            "Sony a7c",
+        price: 1200,
+        discountPrice: 1100,
+        countInStock: 10,
+        sku: "OX-SH-005",
+        category: "Full frame camera",
+        brand: "Sony",
+        collection: "Digital camera",
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=31",
+                altText: "a7c",
+            }
+        ],
+        rating: 4.8,
+        numReviews: 10,
+    },
+    {
+        name: "Fujifilm XT5",
+        description:
+            "Fujifilm XT5",
+        price: 1300,
+        discountPrice: 1000,
+        countInStock: 5,
+        sku: "OX-SH-006",
+        category: "Mirrorless camera",
+        brand: "Sony",
+        collection: "Digital camera",
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=30",
+                altText: "XT5",
+            }
+        ],
+        rating: 4.6,
+        numReviews: 2,
+    },
+]
+
+module.exports = products
