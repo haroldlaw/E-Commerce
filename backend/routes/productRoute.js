27f2af072dc4629af806c6e6f583443a4fcbeb5e
haroldlaw <g00438467@atu.ie>
2025-03-29 15:24:33 +0000

    route to update product details

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -50,4 +50,58 @@ router.post("/", protect, admin, async (req, res) => {
     }
 })
 
+// route to update product details
+router.put("/:id", protect, admin, async (req, res) => {
+    try {
+        const {
+            name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            colours,
+            collection,
+            images,
+            isFeatured,
+            isPublished,
+            weight,
+            sku
+        } = req.body
+
+        // Find product by ID
+        const product = await Product.findById(req.params.id);
+
+        if (product) {
+            // Update product details
+            product.name = name || product.name;
+            product.description = description || product.description;
+            product.price = price || product.price;
+            product.discountPrice = discountPrice || product.discountPrice;
+            product.countInStock = countInStock || product.countInStock;
+            product.category = category || product.category;
+            product.brand = brand || product.brand;
+            product.colours = colours || product.colours;
+            product.collection = collection || product.collection;
+            product.images = images || product.images;
+            product.isFeatured =
+                isFeatured !== undefined ? isFeatured : product.isFeatured;
+            product.isPublished =
+                isPublished !== undefined ? isPublished : product.isPublished;
+            product.weight = weight || product.weight;
+            product.sku = sku || product.sku;
+
+            //   Save the updated product
+            const updatedProduct = await product.save();
+            res.json(updatedProduct);
+        } else {
+            res.status(404).json({ message: "Product not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+})
+
 module.exports = router
