c22a313b7d3e61cb1a9b5d7fdcd84a7485ef2b50
haroldlaw <g00438467@atu.ie>
2025-04-19 16:42:58 +0100

    route to update product in cart

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -77,4 +77,40 @@ router.post("/", async (req, res) => {
     }
 })
 
+// route to update product quantity in cart
+router.put("/", async (req, res) => {
+    const { productId, quantity, guestId, userId } = req.body;
+  
+    try {
+      let cart = await getCart(userId, guestId);
+      if (!cart) return res.status(404).json({ message: "Cart not found" });
+  
+      const productIndex = cart.products.findIndex(
+        (p) =>
+          p.productId.toString() === productId
+      );
+  
+      if (productIndex > -1) {
+        // update quantity
+        if (quantity > 0) {
+          cart.products[productIndex].quantity = quantity;
+        } else {
+          cart.products.splice(productIndex, 1); // Remove product if quantity is 0
+        }
+  
+        cart.totalPrice = cart.products.reduce(
+          (acc, item) => acc + item.price * item.quantity,
+          0
+        );
+        await cart.save();
+        return res.status(200).json(cart);
+      } else {
+        return res.status(404).json({ message: "Product not found in cart" });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Server Error" });
+    }
+  });
+
 module.exports = router
